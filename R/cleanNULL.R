bcb7438b5a48659cafc309657edba7932dbffed6
Simon Goring <goring@wisc.edu>
2023-04-20 06:50:02 +0000

    version 1.0.0

---
+++ R/cleanNULL.R
@@ -0,0 +1,15 @@
+#' @title Clean NULL values
+#' @description Pass an object and convert all
+#' \code{NULL} elements to \code{NA}.
+#' @param x An element that may or may not have NULL values.
+#' @returns parsed `list` where NULL values are changed to NA
+#' @export
+cleanNULL <- function(x) { # nolint
+  out <- rapply(x,
+                function(y) {
+                  #print(y)
+                  ifelse(is.null(y), NA, y)
+                },
+                how = "replace")
+  return(out)
+}
